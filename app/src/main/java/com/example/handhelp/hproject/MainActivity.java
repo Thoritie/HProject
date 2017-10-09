246654db3c7a6145fac70ae408c12ceba3047d3e
Thoritie <thohirh9@gmail.com>
2017-10-09 14:22:36 +0700

    Test the button tap and just appear the messegese :zap:

--- app/src/main/java/com/example/handhelp/hproject/MainActivity.java
+++ app/src/main/java/com/example/handhelp/hproject/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.handhelp.hproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onButton(View v){
+        Toast myToast = Toast.makeText(
+                getApplicationContext(),
+                "Handhelp test ~",
+                Toast.LENGTH_LONG);
+        myToast.show();
+
     }
 }
